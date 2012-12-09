b762dd0c0c862241ffae71243e552b83c6694026
Harry Bock <bock.harryw@gmail.com>
2012-12-09 10:34:41 -0500

    tests: implement basic flash_erase tests
     - Test basic functionality - valid responses (50 blocks erased)
     - Test erase_callback

--- pyatk/tests/test_ramkernel.py
+++ pyatk/tests/test_ramkernel.py
@@ -38,3 +38,43 @@ class RAMKernelTests(unittest.TestCase):
             ver, flash = self.rkl.getver()
             self.assertEqual(ver, imx_version)
             self.assertEqual(flash, flash_model)
+
+    def test_erase(self):
+        """ Test the flash_erase API with no callback specified. """
+        block_size = 0x20000
+
+        for block_index in xrange(50):
+            self.channel.queue_rkl_response(ramkernel.ACK_FLASH_ERASE, block_index, block_size)
+        self.channel.queue_rkl_response(ramkernel.ACK_SUCCESS, 0, 0)
+
+        self.rkl.flash_erase(0x0, 1)
+
+    def test_erase_callback(self):
+        """ Test flash_erase with erase_callback specified. """
+        callback_data = []
+        block_size = 0x20000
+
+        # Queue 50 block erase responses, each with a different block size.
+        # Generally the block size returned by RKL is fixed, but if it is not,
+        # we should be sending the right value.
+        for block_index in xrange(50):
+            self.channel.queue_rkl_response(ramkernel.ACK_FLASH_ERASE,
+                                            block_index,
+                                            block_size + block_index)
+        self.channel.queue_rkl_response(ramkernel.ACK_SUCCESS, 0, 0)
+
+        # Close over callback_data to make sure it is called correctly.
+        def erase_cb(block_idx, block_sz):
+            callback_data.append((block_idx, block_sz))
+
+        # Flash erase doesn't care how many bytes you specify, or the start address.
+        # We're just testing how it handles RKL proto responses.
+        self.rkl.flash_erase(0x0, 1, erase_callback=erase_cb)
+
+        self.assertEqual(len(callback_data), 50)
+        for index, (cb_block_idx, cb_block_sz) in zip(xrange(50), callback_data):
+            self.assertEqual(cb_block_idx, index)
+            self.assertEqual(cb_block_sz, block_size + index)
+
+
+
