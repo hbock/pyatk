e983a77f1c4703519a3be5551af0b556dd523274
Harry Bock <bock.harryw@gmail.com>
2013-03-24 10:37:31 -0400

    flashtool: rename and move from root into bin/

--- bin/flashtool.py
+++ bin/flashtool.py
@@ -22,7 +22,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-Portable Python ATK implementation
+Portable Python command-line tool for bootstrapping i.MX processors.
 """
 
 import os
