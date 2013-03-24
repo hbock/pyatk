033c534888bbe8c78dfb13bd68c9552919215f32
Harry Bock <bock.harryw@gmail.com>
2013-03-24 11:25:31 -0400

    dist: add package distribution support
    tested with py33 and distribute 0.6.27 (I can't upgrade to distribute 0.6.35 with Python 3.3.  This is a known bug with pip).

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='pyatk',
+    version='0.1.0',
+    description='Python implementation of the Freescale bootloader protocol for i.MX processors.',
+    author='Harry Bock',
+    author_email='bock.harryw@gmail.com',
+
+    packages=[
+        'pyatk',
+        'pyatk.channel',
+        'pyatk.tests',
+    ],
+    test_suite='pyatk.tests',
+    scripts=['bin/flashtool.py'],
+    install_requires=[
+        'pyserial >= 2.6',
+        'pyusb >= 1.0.0',
+    ],
+
+    license='BSD',
+    url='https://github.com/hbock/pyatk',
+    long_description="""
+``pyATK`` (Python ATK) is an attempt at replacing the Advanced Toolkit
+(ATK) program distributed by Freescale Semiconductor for their i.MX
+series processors.
+""",
+)
